fb9c241b04849a3f26e9bc99b787288bf626340d
aaronpetzold <197354627+aaronpetzold@users.noreply.github.com>
2026-07-05 15:01:02 +0200

    Finished Week 3

---
+++ week_3/problem_set/dont-panic/hack.sql
@@ -0,0 +1,15 @@
+UPDATE "users" 
+SET "password" = '982c0381c279d139fd221fce974916e7'
+WHERE "username" = 'admin';
+
+DELETE FROM "user_logs" 
+WHERE "new_username" = 'admin' AND "new_password" = '982c0381c279d139fd221fce974916e7';
+
+INSERT INTO "user_logs" ("type", "old_username", "new_username", "old_password", "new_password")
+VALUES (
+    'update',
+    'admin', 
+    'admin', 
+    (SELECT "new_password" FROM "user_logs" WHERE "new_username" = 'admin'), 
+    (SELECT "password" FROM "users" WHERE "username" = 'emily33')
+);
